f6f4af75fa71bf7258c7384503af1160d14529ac
Jared Brown <Jb_90210@hotmail.com>
2018-06-22 16:46:27 -0600

    Implemented authorization for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy, :toggle_status]
+  access all: [:show], user: {except: [:destroy, :new, :Create, :update, :edit]}, admin: :all
   
 
   # GET /items
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
-class PagesController < ApplicationController
-  ##
+class PagesController < ApplicationController  
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   layout "item"
+  
 
   def home
     @items = Item.all
