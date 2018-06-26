4d1948868c62ad2e465d2dee91664378c1377d32
Jared Brown <Jb_90210@hotmail.com>
2018-06-25 19:20:37 -0600

    minor changes to Filter menu, added images to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 3.times do |items|
   Item.create!(
       primary: "#{items}",
-      main_image: "http://via.placeholder.com/200x200",
+      main_image: "http://www.freepngimg.com/thumb/monitor/2-monitor-transparent-lcd-png-image-thumb.png",
       details: "details",
       price: "129.99",
       brand: "Samsung",      
@@ -14,7 +14,7 @@ puts "3 items created"
 3.times do |items|
   Item.create!(
       primary: "#{items}",
-      main_image: "http://via.placeholder.com/200x200",
+      main_image: "http://www.freepngimg.com/thumb/cpu/34178-5-cpu-processor-transparent-background-thumb.png",
       details: "details",
       price: "349.99",
       brand: "Intel",      
@@ -27,7 +27,7 @@ puts "3 items created"
 3.times do |items|
   Item.create!(
       primary: "#{items}",
-      main_image: "http://via.placeholder.com/200x200",
+      main_image: "http://www.freepngimg.com/thumb/keyboard/9-2-keyboard-png-image-thumb.png",
       details: "details",
       price: "39.99",
       brand: "Logitech",      
