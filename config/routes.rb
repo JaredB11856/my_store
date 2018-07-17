e7c3570f38c51a8fbe4b01c9829bbfde516bb34c
Jared Brown <Jb_90210@hotmail.com>
2018-07-16 19:56:17 -0600

    Refactored Carts

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   #get 'cart', to: 'pages#cart'
+  get 'checkout', to: 'carts#checkout'
   get 'user_page', to: 'pages#user_page'
 
   get 'cpus', to: 'categories#cpus'
